0e718c9ae5781d9d4c013a1cf27fd526f6936d9e
Juan David Muñoz Pasquel <jmunozpasquel@gmail.com>
2023-08-10 00:10:23 -0500

    Rectangle_area project accepted

--- categories/geometry/Rectangle_area/src/App.java
+++ categories/geometry/Rectangle_area/src/App.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+import java.util.Vector;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean flag = true;
+        String[] characters;
+        Vector<String> areas = new Vector<>();
+        try (Scanner reader = new Scanner(System.in)) {
+            while (flag) {
+                characters = reader.nextLine().split(" ", 4);
+                if (Integer.valueOf(characters[2]) < Integer.valueOf(characters[0])
+                        || Integer.valueOf(characters[3]) < Integer.valueOf(characters[1])) {
+                    flag = false;
+                    continue;
+                }
+                areas.add(area(Integer.valueOf(characters[2]) - Integer.valueOf(characters[0]),
+                        Integer.valueOf(characters[3]) - Integer.valueOf(characters[1])));
+
+            }
+        }
+        showAreas(areas);
+    }
+
+    private static String area(int length, int high) {
+        return length * high + "";
+    }
+
+    private static void showAreas(Vector<String> areas) {
+        for (String area : areas) {
+            System.out.println(area);
+        }
     }
 }
